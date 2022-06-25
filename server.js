b510fbcf93afd1d4848ae7c50b4dc3407161055e
Emmanuel6093 <elorenzo6093@gmail.com>
2022-06-24 23:32:04 -0400

    deploy

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
 const app = express();
 
 app.use(logger("dev"));
